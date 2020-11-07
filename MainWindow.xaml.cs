20a7ec93054dfe901458dbb9671961c2f9246a7e
Александр Photon74 Колодинский <photon.74@gmail.com>
2020-11-07 22:05:22 +0300

    Мелочи

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,10 +10,10 @@ namespace CalculateStage
     {
         private Calculating calculating = new Calculating();
 
-        private DateTime dateFrom;
-        private DateTime dateTo;
-        public DateTime DateFrom { get { return dateFrom; } }
-        public DateTime DateTo { get { return dateTo; } }
+        private DateTime enrollmentDate;
+        private DateTime dismissalDate;
+        public DateTime EnrollmentDate { get { return enrollmentDate; } }
+        public DateTime DismissalDate { get { return dismissalDate; } }
 
         public MainWindow()
         {
@@ -26,19 +26,22 @@ namespace CalculateStage
             {
                 case 1:
                     {
-                        MessageBox.Show("Неверный формат даты поступления на работу!\nПовторите ввод!");
+                        MessageBox.Show("Неверный формат даты поступления на работу!" +
+                            "\nПовторите ввод!");
                         tbDateFrom.Text = "";
                     };
                     break;
                 case 2:
                     {
-                        MessageBox.Show("Неверный формат даты увольнения с работы!\nПовторите ввод!");
+                        MessageBox.Show("Неверный формат даты увольнения с работы!" +
+                            "\nПовторите ввод!");
                         tbDateTo.Text = "";
                     };
                     break;
                 case 3:
                     {
-                        MessageBox.Show("Вторая дата должна быть больше первой!\nПовторите ввод!");
+                        MessageBox.Show("Вторая дата должна быть больше первой!" +
+                            "\nПовторите ввод!");
                         tbDateFrom.Text = "";
                         tbDateTo.Text = "";
                     }
@@ -57,17 +60,17 @@ namespace CalculateStage
             int years = 0, months = 0, days = 0;
             int totalYears = 0, totalMonths = 0, totalDays = 0;
 
-            calculating.GetDate(tbDateFrom.Text, out dateFrom);
-            if (dateFrom == DateTime.MinValue) Message(1);
+            calculating.GetDate(tbDateFrom.Text, out enrollmentDate);
+            if (enrollmentDate == DateTime.MinValue) Message(1);
             else
             {
-                calculating.GetDate(tbDateTo.Text, out dateTo);
-                if (dateTo == DateTime.MinValue) Message(2);
+                calculating.GetDate(tbDateTo.Text, out dismissalDate);
+                if (dismissalDate == DateTime.MinValue) Message(2);
                 else
-                if (dateFrom > dateTo) Message(3);
+                if (enrollmentDate > dismissalDate) Message(3);
                 else
                 {
-                    calculating.CalculateCurrentStage(DateFrom, DateTo, ref years, ref months, ref days);
+                    calculating.CalculateCurrentStage(EnrollmentDate, DismissalDate, ref years, ref months, ref days);
                     lblCurrentStage.Content = calculating.PrintStage("", years, months, days);
                     calculating.CalculateTotalStage(years, months, days, ref totalYears, ref totalMonths, ref totalDays);
                     lblTotalStage.Content = calculating.PrintStage("", totalYears, totalMonths, totalDays);
